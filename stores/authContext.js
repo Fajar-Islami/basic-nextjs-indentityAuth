d180fd69adcc723736e02f5e4867371b4d28ab10
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-24 16:30:28 +0700

    video 4 - signup & login

--- stores/authContext.js
+++ stores/authContext.js
@@ -13,11 +13,27 @@ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
+    //   Kalau ada user login/sign up di setUser
+    netlifyIdentity.on("login", (user) => {
+      setUser(user);
+      netlifyIdentity.close(); // tutup modal login
+      console.log("login event");
+    });
+
     //  init netlify identity connection
     netlifyIdentity.init(); // membuat koneksi ke netlify
   }, []);
 
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+  const login = () => {
+    //   Untuk membuat modal otomatis dari nelify, ini buka modal
+    netlifyIdentity.open();
+  };
+
+  const context = { user, login };
+
+  return (
+    <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
